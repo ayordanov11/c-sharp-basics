bf3b96591d054d087ada8837eb3baa3bdcdc07a8
Yordanov,Aleksandar A.G <a.yordanov@student.fontys.nl>
2020-03-31 18:27:17 +0200

    D1 Simple operations and calculations: lesson + lab done

---
+++ 2. Simple operations and calculations/SimpleOperationsCalculations - Lab/SimpleOperationsCalculations - Lab/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleOperationsCalculations___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello, " + name);
+            //Console.WriteLine($"Hello {name}" );
+
+            // Task 2
+            //string firstName = Console.ReadLine();
+            //string lastName = Console.ReadLine();
+            //int age = int.Parse(Console.ReadLine());
+            //string town = Console.ReadLine();
+            //Console.WriteLine("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town);
+            //Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}");
+
+            //Task 3
+            //int side = int.Parse(Console.ReadLine());
+            //int result = side * side;
+            ////Console.WriteLine("The result is: {0}", result);
+            //Console.WriteLine("The result is: " + result);
+
+            //Task 4
+            //double number = double.Parse(Console.ReadLine());
+            //double result = number * 2.54;
+            //Console.WriteLine("The result is: " + result);
+
+            //Task 5
+            //Console.Write("Your name: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Number of projects: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int timeForProject = 3;
+            //int result = timeForProject * number;
+
+            //Console.WriteLine("The architect " + name + " will need " + result + " hours to complete " + number + " projects");
+
+            //Task 6
+            //Console.Write("Number of dogs is: ");
+            //int dogsNumber = int.Parse(Console.ReadLine());
+            //Console.Write("Number of other animals: ");
+            //int otherAnimals = int.Parse(Console.ReadLine());
+            //double dogFood = 2.50;
+            //double otherAnimalFood = 4;
+
+            //double priceFoodDogs = dogsNumber * dogFood;
+            //double priceFoodOtherAnimals = otherAnimals * otherAnimalFood;
+            //double finalResult = priceFoodDogs + priceFoodOtherAnimals;
+
+            //Console.WriteLine("The final result is: " + finalResult);
+
+            //Task 7
+            //Console.Write("How many square meters: ");
+            //double squareMeters = double.Parse(Console.ReadLine());
+            //double wholeGarden = squareMeters * 7.61;
+            //double discount = 0.18 * wholeGarden;
+            //double result = wholeGarden - discount;
+
+            //Console.WriteLine("The final price is: " + Math.Round(result,2));
+            //Console.WriteLine("The discont is: " + Math.Round(discount,2));
+
+            //Task 8
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            int volume = length * width * height; //cm3
+            //1dm = 10cm
+            //1dm3 = 10 * 10 * 10 cm3
+
+            double totalLiters = volume * 1.0 / 1000;
+
+            // 100 - 17/100*100 = 83;
+            // 100 * 0.83
+
+            double liters = totalLiters - (percent / 100) * totalLiters;
+
+            Console.WriteLine($"{liters:f3}");
+        }
+    }
+}
---
+++ 2. Simple operations and calculations/SimpleOperationsCalculations/SimpleOperationsCalculations/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleOperationsCalculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // CTRL + K + C = comment
+            // CTRL + K + U = uncomment
+            // CTRL + K + D = format code
+
+            // DEMO LIVE
+
+            //int num = int.MaxValue;  //2147483648;
+            //double num2 = 5.5;
+            //string name = "Plamen";
+            //char symbol = ' ';
+            //bool check = false;
+
+            //Read from the console //Console.ReadLine always returns string
+            //string name = Console.ReadLine();
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine(name);
+            //Console.WriteLine(age);
+
+            //Registration example
+            //Console.Write - it remains on the same row
+            //Console.Write("First name:");
+            //string firstName = Console.ReadLine();
+            //Console.Write("Last name:");
+            //string lastName = Console.ReadLine();
+            //Console.Write("Email:");
+            //string email = Console.ReadLine();
+            //Console.Write("Birth year:");
+            //int birthYear = int.Parse(Console.ReadLine());
+            //Console.Write("Username:");
+            //string username = Console.ReadLine();
+            //Console.Write("Password:");
+            //string password = Console.ReadLine();
+            //Console.Write("Money:");
+            //double money = double.Parse(Console.ReadLine());
+
+            //string age = "30";
+            //int ageAfter7Years = int.Parse(age) + 7;
+            //Console.WriteLine(ageAfter7Years);
+
+            //string firstName = "Alex";
+            //string lastName = "Yordanov";
+            //Console.WriteLine("Hi, " + firstName + " " + lastName);
+
+            //int age = 23;
+            //Console.WriteLine("I am " + age + " years old.");
+
+            //5 - 2 = 3 for example
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int result = a - b;
+            //Console.WriteLine(result);
+
+            //int a = 5;
+            //int b = 7;
+            //int product = a * b; // 35
+
+            //one of the data type should be double to see the correct answer
+            //double numOfCookies = 5;
+            //int numOfPeople = 2;
+            //Console.WriteLine(numOfCookies / numOfPeople);
+
+            //We can't divide by 0; //DIVIDE BY ZERO Exception
+            //int a = 5;
+            //int b = 0;
+            //Console.WriteLine(a / b);
+
+            // 25 people
+            // 53 cookies
+            // 1 lecturer
+            // 53 = 25 * 2 + 3
+            //int people = 25;
+            //int cookies = 53;
+            //Console.WriteLine(cookies % people); // The answer is 3, the remainder = остатъка.
+
+            //Console.WriteLine(4 % 2); // 0
+
+            //string firstName = "Alex";
+            //string lastName = "Yordanov";
+            //string town = "Varna";
+
+            ////I am Alex Yordanov from Varna
+            //// interpolation
+            //Console.WriteLine($"I am {firstName} {lastName} from {town}");
+
+            //Interpolation another example
+            //string firstName = Console.ReadLine();
+            //string lastName = Console.ReadLine();
+            //int age = int.Parse(Console.ReadLine());
+            //string town = Console.ReadLine();
+            //Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+
+            ////MATH library
+            //double num = 5.678954;
+            //Console.WriteLine(Math.Round(num, 2)); //6; 6.68 - втори знак след запетаята
+
+            //double num = 5.456; 
+            //Console.WriteLine(Math.Ceiling(num)); // -> 6 always up!
+            //Console.WriteLine(Math.Floor(num)); // -> 5 always low!
+
+            //Console.WriteLine((Math.PI)); //PI
+
+            //Console.WriteLine(Math.Abs(-50)); // -> 50 always positive in output
+
+            double num = 5.567;
+            //Formatting the number
+            Console.WriteLine($"num = {num:f2}"); // num = 5.57;
+        }
+    }
+}
