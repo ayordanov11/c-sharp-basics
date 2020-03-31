3bcbdb79b1d2b5d658f49e2b3f96037ce694df9d
Yordanov,Aleksandar A.G <a.yordanov@student.fontys.nl>
2020-03-31 19:30:18 +0200

    D1 Simple Operations and Calculations Exercise Test

---
+++ 2. Simple operations and calculations/SimpleOperationsCalculations - Exercise/2. RadiansToDegrees/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK 2
+            //1.radians => read
+            //2. degrees = radians * 180 / PI
+            //3. print degrees
+
+            Console.Write("Type the radians: ");
+            double radians = double.Parse(Console.ReadLine());
+            double degrees = radians * 180 / Math.PI;
+            Console.WriteLine(Math.Round(degrees));
+        }
+    }
+}
---
+++ 2. Simple operations and calculations/SimpleOperationsCalculations - Exercise/SimpleOperationsCalculations - Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleOperationsCalculations___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int 7 => double 7.00
+            //int 12 = > double 12.00
+            //int number1 = int.Parse(Console.ReadLine());
+            //int number2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(number1 * 1.0 / number2);
+
+            //TASK 1
+            Console.Write("Dollars: ");
+            double dollars = double.Parse(Console.ReadLine());
+
+            double leva = dollars * 1.79549;
+
+            //Console.WriteLine("The amount in bulgarian leva is : " + Math.Round(leva, 2));
+            Console.WriteLine($"The amount in bulgarian leva is: {leva:f2}");
+
+            ////TASK 2
+            ////1.radians => read
+            ////2. degrees = radians * 180 / PI
+            ////3. print degrees
+
+            //Console.Write("Type the radians: ");
+            //double radians = double.Parse(Console.ReadLine());
+            //double degrees = radians * 180 / Math.PI;
+            //Console.WriteLine(Math.Round(degrees));
+        }
+    }
+}
