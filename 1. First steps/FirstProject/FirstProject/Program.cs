e962fa45249c21ce7a46f96239800249cffa3ce5
Yordanov,Aleksandar A.G <a.yordanov@student.fontys.nl>
2020-03-31 13:53:57 +0200

    D1 First project

---
+++ 1. First steps/FirstProject/FirstProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SoftUni!");
+
+            Console.WriteLine(5);
+
+            Console.WriteLine(5 + 5);
+
+            //Console.ReadLine();
+
+            // H O M E W O R K
+
+            // Task 2
+            Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+
+            //Task 3
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
+            Console.WriteLine(10);
+
+            //Task 4
+            int a = 2;
+            int b = 7;
+
+            int area = a * b;
+            Console.WriteLine(area);
+
+            //Task 5
+            int c = 5;
+            int d = 3;
+            int result = c * d;
+            Console.WriteLine(result);
+
+        }
+    }
+}
